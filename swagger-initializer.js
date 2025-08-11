7238675ac689448b5ceced5615fd2fb2f6866380
habin81111 <habin81111@naver.com>
2025-08-11 12:41:25 +0900

    [feat] update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: './swagger.json?v=' + Date.now(),
+    url: "https://raw.githubusercontent.com/Unithon013/api-docs/refs/heads/main/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
